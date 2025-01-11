b724637b1afc0e9902768019a0a627d4cc88f66a
Aniket Dhangar <147258918+AniketDhangar@users.noreply.github.com>
2025-01-11 11:19:25 +0530

    new files

---
+++ 22_practice.js
@@ -0,0 +1,12 @@
+// Write a function that if you give a number as input it will square that number and return that square.
+// That is, if you give 5 as input, it will get 25 as output squared. 
+
+
+
+
+function squareTheNum(num){
+
+    return num*num;
+}
+
+console.log(squareTheNum(5));
---
+++ 23_practice.js
@@ -0,0 +1,31 @@
+// Define an object named pizza as in the following example:
+
+
+
+// const pizza = {
+
+//   toppings: ['cheese', 'sauce', 'pepperoni'],
+
+//   crust: 'deep dish',
+
+//   Serves: 2
+
+// }
+
+// and print pepperoni. */
+
+
+
+
+const pizza = {
+
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+  
+    crust: 'deep dish',
+  
+    Serves: 2
+  
+  };
+
+  console.log(pizza.toppings[2]);
+  
